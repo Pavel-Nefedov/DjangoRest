6387443ed51ac1ae0eb2337a57148dc443068339
pashka <pavel.nefyodov.74@mail.ru>
2022-08-30 22:19:16 +0500

    add lesson_5

--- service/usersapp/views.py
+++ service/usersapp/views.py
@@ -1,8 +1,11 @@
+from rest_framework.authentication import TokenAuthentication
 from rest_framework.generics import ListAPIView, RetrieveAPIView, UpdateAPIView
 from rest_framework.renderers import JSONRenderer
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from .models import User
 from .serializers import UserModelSerializer
+
 
 
 class UserViewSet(ModelViewSet):
@@ -26,4 +29,6 @@ class UserUpdateApiView(UpdateAPIView):
     renderer_classes = [JSONRenderer]
     queryset = User.objects.all()
     serializer_class = UserModelSerializer
+    premission_classes = (IsAuthenticated,)
+    # authentication_classes = (TokenAuthentication,)
 
